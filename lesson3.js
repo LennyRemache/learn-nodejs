21f5efc77ef2e9e8014233bace9cda808dc502fa
LennyRemache <lenny.j.remache@gmail.com>
2023-01-13 19:37:43 -0500

    FS (async) Built-in Module

--- lesson3.js
+++ lesson3.js
@@ -35,10 +35,43 @@ const absolute = path.resolve(__dirname, "c");
 
 /* FS Module (Sync) */
 
-const { readFileSync, writeFileSync } = require("fs");
+const { readFileSync, writeFileSync, write } = require("fs");
 
 const first = readFileSync("./lesson3-first.txt", "utf8");
 const second = readFileSync("./lesson3-second.txt", "utf8");
 console.log(first, second);
 
 writeFileSync("lesson3-third.txt", "Hi this is third.txt", { flag: "a" }); // flag to prevent override and instead append to file
+
+/* FS Module (async) */
+// needs a callback
+const { readFile, writeFile } = require("fs");
+
+readFile("./lesson3-first.txt", "utf8", (err, res) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = res;
+
+  readFile("./lesson3-second.txt", "utf8", (err, res) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = res;
+
+    writeFile(
+      "lesson3-third-async.txt",
+      `Async Writing ${first} ${second}`,
+      { flag: "a" },
+      (err, res) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(res);
+      }
+    );
+  });
+});
